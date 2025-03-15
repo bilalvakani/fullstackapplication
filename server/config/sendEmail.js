8e1fce0b86bcc7d803b6e17ced28db8b182485b3
Bilal-dev <143352356+bilalvakani@users.noreply.github.com>
2025-03-15 05:05:37 -0700

    refactor sendEmail.js to comment out unused email sending logic and improve code clarity

--- server/config/sendEmail.js
+++ server/config/sendEmail.js
@@ -28,3 +28,42 @@ const sendEmail = async ({sendTO, subject, html}) => {
 
 
 export default sendEmail;
+// import { Resend } from 'resend';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// if(!process.env.RESEND_API) {
+//   console.log('RESEND_API is required');
+// }
+
+// const resend = new Resend(process.env.RESEND_API);
+
+
+// const sendEmail = async ({ sendTO, subject, html }) => {
+//     try {
+//         // Allowed email address for testing
+//         const allowedEmail = 'useo0969@gmail.com'; // Bilal ki email address yahan daalain
+
+//         // Agar sendTO yeh email hai, to bhejna allow karain
+//         if (sendTO !== allowedEmail) {
+//             return console.error('Aap sirf testing email address par emails bhej sakte hain.');
+//         }
+
+//         // Email send karna
+//         const { data, error } = await resend.emails.send({
+//             from: 'bilalVakani <onboarding@resend.dev>', // Ye aapka test email address hai
+//             to: sendTO,
+//             subject: subject,
+//             html: html,
+//         });
+
+//         if (error) {
+//             return console.error({ error });
+//         }
+
+//         return data; // Agar email bhej diya gaya ho to data return karain
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+// export default sendEmail;
