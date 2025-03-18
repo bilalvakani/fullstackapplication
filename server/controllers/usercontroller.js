45a019d192de2efe43d4c7a7c38fec62e861e1f4
Bilal-dev <143352356+bilalvakani@users.noreply.github.com>
2025-03-18 06:31:37 -0700

    add logout functionality to user controller with cookie clearing

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -180,3 +180,35 @@ try {
     });
 }
 }
+
+//  LOGUT API
+
+export  async function logoutController(request,response){
+    try {
+        const cookiesOption ={
+            httpOnly:true,
+            secure:true,
+            sameSite:"None"
+        }
+    
+        response.clearCookie("acceessToken",cookiesOption)
+        response.clearCookie("refreshToken",cookiesOption)
+
+        return response.json({
+            message:"Logout Succeessfully",
+            error:false,
+            success:true
+
+        })
+
+        
+    } catch (error) {
+        return response.status(500).json({
+            message:error.message||error,
+            error:true,
+            success:false
+
+        })
+        
+    }
+}
