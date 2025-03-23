d6ff8a47d0be651c0b84f06fbbca9cedb78375c7
Bilal-dev <143352356+bilalvakani@users.noreply.github.com>
2025-03-23 07:03:45 -0700

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,9 +1,25 @@
 import jwt from 'jsonwebtoken';
 
-const auth = (request, response, next) => {
+const auth = async(request, response, next) => {
   try {
     const token = request.cookies.accessToken || request?.header?.authorization?.split(" ")[1];
-    console.log("token",token)
+    // console.log("token",token)
+
+    if (!token){
+        return response.json(401).json({
+            message :"provide Token"
+
+        })
+
+    }
+
+    const decode =  await jwt.verify(token,Process.env.SECRET_KEY)
+  console.log("decode", decode)
+
+
+
+
+
   } catch (error) {
     return response.status(500).json({
       message: error.message || error,
