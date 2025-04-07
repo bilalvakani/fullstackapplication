ccf444d332ffa231d2a77768649a27c3790c5f21
Bilal-dev <143352356+bilalvakani@users.noreply.github.com>
2025-04-07 01:27:43 -0700

    fix: correct access token spelling and improve logout functionality; add auth middleware to logout route

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      App
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </main>
+ 
+  )
+}
+
+export default App
